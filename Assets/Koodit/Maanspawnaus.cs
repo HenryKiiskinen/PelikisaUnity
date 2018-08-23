ac8d21e86473dfa8dc1776f718fce3abdd35e65b
HenryKiiskinen <kiiskinen.henry2@gmail.com>
2018-08-23 16:15:08 +0300

    First commit

---
+++ Assets/Koodit/Maanspawnaus.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent (typeof(SpriteRenderer))]
+public class Maanspawnaus : MonoBehaviour {
+
+    public int offsetX = 2;
+    public bool hasARightBuddy = false; 
+    public bool hasALeftBuddy = false;
+    public bool reverseScale = false;
+  
+
+    private float spriteWidth = 0f;
+    private Camera cam;
+    private Transform mTransform;
+
+
+
+
+
+
+
+
+
+    private void Awake()
+    {
+        cam = Camera.main;
+        mTransform = transform;
+       
+}
+
+	void Start () {
+        SpriteRenderer sRenderer = GetComponent<SpriteRenderer>();
+        spriteWidth = sRenderer.sprite.bounds.size.x;
+
+	} //start
+	
+	public void Update () {
+        // vieläkö spawnataan lisäää(tarkistus)
+        if (hasALeftBuddy == false || hasARightBuddy == false)
+        {
+            float camHorizontaalinenLisaus = cam.orthographicSize * Screen.width / Screen.height;
+
+            // lasketaan x akselin kohta kameran näkemästä alueesta
+            float reunaOikea = (mTransform.position.x + spriteWidth / 2) - camHorizontaalinenLisaus;
+
+            float reunaVasen = (mTransform.position.x - spriteWidth / 2) + camHorizontaalinenLisaus;
+
+            if (cam.transform.position.x >= reunaOikea - offsetX && hasARightBuddy == false)
+            {
+                MakeNewBuddy(1);
+                hasARightBuddy = true;
+            }
+            else if (cam.transform.position.x <= reunaVasen + offsetX && hasALeftBuddy == false)
+            {
+                MakeNewBuddy(-1);
+                hasALeftBuddy = true;
+            }
+
+
+     
+        }
+
+    }//update
+    void MakeNewBuddy(int oikeaVasen)//tarkistetaan kummalle puolella lisätään seinää
+    {
+        Vector3 newPosition = new Vector3(mTransform.position.x + mTransform.localScale.x * spriteWidth * oikeaVasen, mTransform.position.y, mTransform.position.z);
+        Transform uusi =  (Transform)Instantiate (mTransform, newPosition, mTransform.rotation);
+        if (reverseScale == true)
+        {
+            uusi.localScale = new Vector3 (uusi.localScale.x * -1, uusi.localScale.y, uusi.localScale.z);
+        }
+        uusi.parent = mTransform.parent;
+        if (oikeaVasen > 0)
+        {
+            uusi.GetComponent<Maanspawnaus>().hasALeftBuddy = true;
+
+        }else
+        {
+            uusi.GetComponent<Maanspawnaus>().hasARightBuddy = true;
+        }
+    }
+}//class
---
+++ Assets/Koodit/Parallax.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+
+
+    public Transform[] Tausta;     // arraay taustoillle 
+    private float[] parallaxScales; // liikkumiis osuus kameralle
+    public float smoothing = 1f; // pehmennetään liikettä
+
+    private Transform cam; //pääkameran viite
+    private Vector3 previousCamPos; // kameran aiempi paikka x, y ja z akselieilla
+
+    void Awake()
+    {
+        cam = Camera.main.transform;
+
+    }
+
+
+	void Start () {
+        //Edellisen framin kamera paikka = nykyinen
+        previousCamPos = cam.position;
+        
+        parallaxScales = new float[Tausta.Length];
+
+            for (int i = 0; i < Tausta.Length; i++) //käydään kaikki Tausta läpi
+        {
+            parallaxScales[i] = Tausta[i].position.z * -1;
+        }
+        	
+	}//start
+	
+
+	void Update () {
+        for (int i = 0; i < Tausta.Length; i++) { //Tausta läpi
+            float parallax = (previousCamPos.x - cam.position.x) * parallaxScales[i]; //kameroiden paikkojen erolla tehdään parallax efekti
+
+            float TaustaargetPosX = Tausta[i].position.x + parallax;
+            Vector3 TaustaargetPos = new Vector3 (TaustaargetPosX, Tausta[i].position.y, Tausta[i].position.z);
+
+            // fadaus 
+            Tausta[i].position = Vector3.Lerp(Tausta[i].position, TaustaargetPos, smoothing * Time.deltaTime);
+
+        }
+
+        previousCamPos = cam.position;
+	}//update
+}//class
